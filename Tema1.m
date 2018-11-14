873b22b48a36455fb90aa92323de8339dfc14e81
krax99 <44239460+krax99@users.noreply.github.com>
2018-11-14 20:35:03 +0200

    Update Tema1.m

--- Tema1.m
+++ Tema1.m
@@ -1,14 +1,15 @@
+figure 1
 t = 0:0.002:2;      
 x = square(t, 25);       
 plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Square Periodic Wave'),grid;
 
-
+figure 2
 t = 0:0.02:2;    
 x = square(t, 25);
 plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Square Periodic Wave'),grid;
 
 
-
+figure 3
 t = 0:0.2:2;   
 x = square(t, 25);
 plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Square Periodic Wave'),grid;
