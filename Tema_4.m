639c64419316a4e1a2065e26b7b2718a5cc11ade
krax99 <44239460+krax99@users.noreply.github.com>
2018-11-14 20:41:13 +0200

    Update Tema_4.m

--- Tema_4.m
+++ Tema_4.m
@@ -1,6 +1,23 @@
+figure 1
 A = 0.8;    
 T = 3;
 F = 1 / T;
 t = 0:0.002:3; 
+s = A*sin(2*pi*F*t);  
+plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine');
+
+figure 2
+A = 0.8;    
+T = 3;
+F = 1 / T;
+t = 0:0.02:3; 
+s = A*sin(2*pi*F*t);  
+plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine');
+
+figure 3
+A = 0.8;    
+T = 3;
+F = 1 / T;
+t = 0:0.2:3; 
 s = A*sin(2*pi*F*t);  
 plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine');
