9637614fda22c046cc9175e60d809e2c4f4afa6d
krax99 <44239460+krax99@users.noreply.github.com>
2018-11-14 20:36:01 +0200

    Update E6.m

--- E6.m
+++ E6.m
@@ -1,6 +1,16 @@
- s=rand(1,10)>0.5;
- t=0.5;
- f = 12000;
- N = t * f;
- n = 0:N-1;
-plot(n,s)
+  x = rand(1,6);
+
+  x(x>0.5)=1;
+  x(x<0.5)=0;
+  
+  t=repmat(x,6,1)
+  t=t(1:36);
+  T=0.5
+  Fs=12;
+  Ts=1/Fs;
+  NR_ESANTIOANE=T/Ts
+  
+  n=0:NR_ESANTIOANE-1
+
+  stairs(t);
+
