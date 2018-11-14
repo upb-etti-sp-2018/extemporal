4886b38323e67f30828a85e31d14df7eb1c89f2c
krax99 <44239460+krax99@users.noreply.github.com>
2018-11-14 20:38:18 +0200

    Update Tema_2.m

--- Tema_2.m
+++ Tema_2.m
@@ -1,3 +1,15 @@
+figure 1
 t = 0:0.002:5;      
+x = sawtooth (2 * pi * 140 * t , 0.5);    
+plot(t,x),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Triangular Periodic Wave'),grid;
+
+figure 2
+t = 0:0.02:5;      
+x = sawtooth (2 * pi * 140 * t , 0.5);    
+plot(t,x),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Triangular Periodic Wave'),grid;
+
+
+figure 3
+t = 0:0.2:5;      
 x = sawtooth (2 * pi * 140 * t , 0.5);    
 plot(t,x),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Triangular Periodic Wave'),grid;
