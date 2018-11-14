08aca6b5b806a70e7973b90d5d48ef0bbf38e250
krax99 <44239460+krax99@users.noreply.github.com>
2018-11-14 20:42:06 +0200

    Update Tema_5.m

--- Tema_5.m
+++ Tema_5.m
@@ -1,6 +1,23 @@
+figure 1
 A = 1.5;    
 T = 4;
 F = 1 / T;
 t = 0:0.002:4;    
+s = A*sin(2*pi*F*t);    
+plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Sine Periodic Wave'),grid;
+
+figure 2
+A = 1.5;    
+T = 4;
+F = 1 / T;
+t = 0:0.02:4;    
+s = A*sin(2*pi*F*t);    
+plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Sine Periodic Wave'),grid;
+
+figure 3
+A = 1.5;    
+T = 4;
+F = 1 / T;
+t = 0:0.2:4;    
 s = A*sin(2*pi*F*t);    
 plot(t,s,'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Sine Periodic Wave'),grid;
